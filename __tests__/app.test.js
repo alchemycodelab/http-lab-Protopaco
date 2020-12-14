5d23633915839a40656431eff609b565d2ba6543
Paul Stevens <twinkisan@hotmail.com>
2020-12-14 14:36:18 -0800

    / GET route & test pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,4 +3,13 @@ const app = require('../lib/app');
 
 describe('app routes', () => {
 
+    it('tests / GET route, expectes response "hi"', async () => {
+
+        const response = await request(app)
+            .get('/');
+
+        expect(response.text).toEqual('hi');
+
+    })
+
 });
