39712c79ada3e564b7905ffc537f355224ff9b9c
Paul Stevens <twinkisan@hotmail.com>
2020-12-14 14:40:12 -0800

    adds  /echo POST test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,22 @@ const app = require('../lib/app');
 
 describe('app routes', () => {
 
-    it('tests / GET route, expectes response "hi"', async () => {
+    it('tests / GET route, expects response "hi"', async () => {
 
         const response = await request(app)
             .get('/');
 
         expect(response.text).toEqual('hi');
 
+    });
+
+    it('tests /GET route, expects status code 200 && plain text request body', () => {
+        const text = 'plain text'
+        const response = await request(app)
+            .post('/echo')
+            .send(text);
+
+        expect(response.body).toEqual(text);
     })
 
 });
