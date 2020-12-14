2248791999fe0013c30e63489b67aad90c95f02c
Paul Stevens <twinkisan@hotmail.com>
2020-12-14 15:01:02 -0800

    adds colored GET tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,13 +12,35 @@ describe('app routes', () => {
 
     });
 
-    it('tests /GET route, expects status code 200 && plain text request body', () => {
+    it('tests /echo POST route, expects status code 200 && plain text request body', async () => {
         const text = 'plain text'
         const response = await request(app)
             .post('/echo')
             .send(text);
 
-        expect(response.body).toEqual(text);
+        expect(response.text).toEqual(text);
+        expect(response.status).toEqual(200);
+    })
+
+    it('tests /red GET route, expects "<hi>red</h1>"', async () => {
+        const response = await request(app)
+            .get('/red')
+
+        expect(response.text).toEqual("<h1>red</h1>");
+    })
+
+    it('tests /green GET route, expects "<h1>green</h1>"', async () => {
+        const response = await request(app)
+            .get('/green')
+
+        expect(response.text).toEqual("<h1>green</h1>");
+    })
+
+    it('tests /blue GET route, expects "<h1>blue</h1>', async () => {
+        const response = await request(app)
+            .get('/blue')
+
+        expect(response.text).toEqual("<h1>blue</h1>");
     })
 
 });
